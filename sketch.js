b44f498d65f40d3d9330749805949018d834eea0
Jamalious <johnynana12@gmail.com>
2025-11-19 09:55:16 -0600

    multiplayer setup

--- sketch.js
+++ sketch.js
@@ -4,13 +4,40 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let shared, guests, my;
+let test;
+
+class Player {
+  constructor(x, y, id, color, direction, isAlive, trail, base){
+    this.x = x;
+    this.y = y;
+    this.id = id;
+    this.color = color;
+    this.direction  = direction;
+    this.base = base;
+    this.trail = trail;
+    this.isAlive = isAlive;
+    
+  }
+  
+}
 
 
+function preload(){
+  partyConnect("wss://deepstream-server-1.herokuapp.com","grid.io");
+  shared = partyLoadShared("shared");
+  guests = partyLoadGuestShareds();
+  my = partyLoadMyShared();
+
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  test = new Player(50, 50, Math.floor(random() * 1000000, "green", "right", "true", [], [] ));
+  console.log(test);
 }
 
 function draw() {
-  background(220);
+  background(0);
   circle(mouseX, mouseY,50);
 }
+
