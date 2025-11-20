6d58036cf098f7243d2ce784c58205ed8ff2c37b
Jamalious <johnynana12@gmail.com>
2025-11-20 09:36:28 -0600

    drawPlayer function

--- sketch.js
+++ sketch.js
@@ -8,10 +8,10 @@ let shared, guests, my;
 let test;
 
 class Player {
-  constructor(x, y, id, color, direction, isAlive, trail, base){
+  constructor(x, y, color, direction, isAlive, trail, base){
     this.x = x;
     this.y = y;
-    this.id = id;
+    this.id = Math.floor(Math.random(1000));
     this.color = color;
     this.direction  = direction;
     this.base = base;
@@ -19,25 +19,48 @@ class Player {
     this.isAlive = isAlive;
     
   }
+  update(){
+    if(this.isAlive){
+    }
+  }
+
   
 }
 
 
 function preload(){
   partyConnect("wss://deepstream-server-1.herokuapp.com","grid.io");
-  shared = partyLoadShared("shared");
+  shared = partyLoadShared("shared", {
+    playerPerspective: 3,
+    
+  });
   guests = partyLoadGuestShareds();
   my = partyLoadMyShared();
 
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  test = new Player(50, 50, Math.floor(random() * 1000000, "green", "right", "true", [], [] ));
+  test = new Player(0,0,0,0,0,0,0,0);
   console.log(test);
 }
 
 function draw() {
   background(0);
   circle(mouseX, mouseY,50);
+}
+
+function drawPlayers(){
+  for(let g of guests) {
+    if(g.player !== my.player){
+      push();
+      drawNewPlayer(g.player.x, g.player.y, g.player.color, g.player.direction, g.player.isAlive, g.player.trail, g.player.base);
+      pop();
+    }
+  }
+}
+function drawNewPlayer(x, y, id, color, direction, isAlive, trail, base){
+  if (isAlive){
+  }
+
 }
 
