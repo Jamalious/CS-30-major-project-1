76b1644e1982c586d1d6c0017809e32f7fd635a2
Jamalious <johnynana12@gmail.com>
2025-11-21 09:55:04 -0600

    Linear Interpolation for smoother movement

--- sketch.js
+++ sketch.js
@@ -4,13 +4,18 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
+const SPEED = 2;
 let shared, guests, my;
 let test;
+
 
 class Player {
   constructor(x, y, color, direction, isAlive, trail, base){
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
     this.id = Math.floor(Math.random(1000));
     this.color = color;
     this.direction  = direction;
@@ -20,13 +25,20 @@ class Player {
     
   }
   update(){
-    if(this.isAlive){
-    }
+    my.pos.dx = lerp(my.pos.dx, i.x * SPEED, 0.2);
+    my.pos.dy = lerp(my.pos.dy, i.y * SPEED, 0.2);
+    my.pos.x += my.pos.dx;
+    my.pos.y += my.pos.dy;
+    playerIsDead();
+
   }
 
   
 }
 
+function checkCollisions(playerX, playerY){
+
+}
 
 function preload(){
   partyConnect("wss://deepstream-server-1.herokuapp.com","grid.io");
@@ -40,7 +52,7 @@ function preload(){
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  test = new Player(0,0,0,0,0,0,0,0);
+  my.player = new Player(0,0,0,0,0,0,0);
   console.log(test);
 }
 
@@ -63,4 +75,34 @@ function drawNewPlayer(x, y, id, color, direction, isAlive, trail, base){
   }
 
 }
+function updatePlayer() {
+  my.player.update();
+}
+function movePlayer() {
+  if (my.isAlive){
+    my.pos.dx = lerp(my.pos.dx, i.x * SPEED);
+  }
+}
 
+//disc room clone input functions
+function moveLeft(){
+  return keyIsDown(LEFT_ARROW) || keyIsDown(65) || keyIsDown(74);
+}
+function moveRight(){
+  return keyIsDown(RIGHT_ARROW) || keyIsDown(68) || keyIsDown(76);
+}
+function moveUp(){
+  return keyIsDown(UP_ARROW) || keyIsDown(87) || keyIsDown(73);
+}
+function moveDown(){
+  return keyIsDown(DOWN_ARROW) || keyIsDown(83) || keyIsDown(75);
+}
+function horizontalMovement(){
+  return moveRight() - moveLeft();
+}
+function verticalMovement(){
+  return moveDown() - moveUp();
+}
+function input(){
+  return {x: verticalMovement(), y: verticalMovement};
+}
